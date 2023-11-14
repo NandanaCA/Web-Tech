012eebb0762876941d99a8cb5ed5872931f75f75
NandanaCA <141863296+NandanaCA@users.noreply.github.com>
2023-11-14 15:38:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes,Route } from "react-router-dom"
+import {Home} from './components/Home'
+import {Report} from './components/Report'
+import {Trends} from './components/Trends'
+import {Map} from './components/Map'
+
+
+function App()
+{
+  return(
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='Report' element={<Report/>}></Route>
+      <Route path='Trends' element={<Trends/>}></Route>
+      <Route path='Map' element={<Map/>}></Route>
+    </Routes>
+  )
+}
+export default App
