f2fe417bde7ea00c0dbb59655c226e089908ce49
NandanaCA <141863296+NandanaCA@users.noreply.github.com>
2023-11-14 16:10:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import {Home} from './components/Home'
 import {Report} from './components/Report'
 import {Trends} from './components/Trends'
 import {Map} from './components/Map'
+import {Navbar} from './components/Navbar'
 
 
 function App()
 {
   return(
+    <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='Report' element={<Report/>}></Route>
       <Route path='Trends' element={<Trends/>}></Route>
       <Route path='Map' element={<Map/>}></Route>
     </Routes>
+    </>
   )
 }
 export default App
