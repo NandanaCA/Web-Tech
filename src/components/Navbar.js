0557d13539ebbafd8f8bda92dc4367778e073fdf
NandanaCA <141863296+NandanaCA@users.noreply.github.com>
2023-11-14 16:10:10 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ export const Navbar=()=> {
     return (
         <nav>
             <Link to='/'>Home</Link>
-            <Link to='/About'>About</Link>
+            <Link to='/Report'>Report</Link>
+            <Link to='/Trends'>Trends</Link>
+            <Link to='/Map'>Map</Link>
         </nav>
     )
 }
